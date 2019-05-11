bed1f8d7c7113a174bfdc97e3b157a7f121ea6a2
Hunter315 <35641380+Hunter315@users.noreply.github.com>
2019-05-11 00:59:05 -0600

    Replace blank slate with interactive particles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,25 +1,15 @@
 import React from "react";
 import { Link } from "react-scroll";
 import { HeroContainer } from "./Hero.style";
+import Particles from '../Particles/particles'
 
 const Hero = () => {
   return (
     <HeroContainer id="home">
       <div className="hero-image">
-      {/* <img src={require("../../assets/img/grit.png")} 
-      alt="grit" 
-      style={{position: "absolute", }}
-      /> */}
-        {/* <img
-        style={{height: "100%", minWidth: "100%"}}
-          src={require("../../assets/img/hntrnotext.png")}
-          alt="Hunter Smith"
-        /> */}
-        <div style={{height: "100%", width: "100%", padding: "0 10%", position: "absolute", boxSizing: "border-box", top: "0"}}>
-        <h1 className="grit" style={{position: "absolute", margin: "0",  color: "white",  top:"20%", fontSize:"140px", lineHeight: "8rem"}}>HUNTER SMITH</h1>
+      <Particles />
 
-        <h1 className="grit" style={{position: "absolute", margin: "0",  color: "white", right:"50px", bottom: "10%", fontSize:"70px", zIndex:"3"}}>SOFTWARE DEVELOPER</h1>
-        </div>
+        
       </div>
       <div className="drop-down-icon">
         <Link
